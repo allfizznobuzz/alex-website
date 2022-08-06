b49030fbfc2ac4820e4f15fcc55c452dc9553130
john.h95@gmail.com <Hur2dle1!>
2022-08-05 21:39:21 -0400

    Removed files that were ned needed

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Legal Nurse Elites',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'RN services provided for all legal needs' },
     {
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
